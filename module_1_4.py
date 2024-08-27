6ddbe44ed383d1efee8ec6b642b4f7daba411b79
demart-nitfoll <demfsg27@gmail.com>
2024-08-27 14:59:22 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,2 @@
+my_string = input('Введите что-нибудь: ')
+print(len(my_string),'\n',my_string.upper(),'\n', my_string.lower(),'\n', my_string.replace(' ', ''), '\n',my_string[0], '\n',my_string[len(my_string)-1])
