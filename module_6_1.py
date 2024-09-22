d195bab5212b8bea02e68a1b71aaa361ccf43af3
demart <demfsg27@gmail.com>
2024-09-22 16:56:01 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,44 @@
+class Plant:
+    def __init__(self, name):
+        self.edible = False
+        self.name = name
+
+class Animal:
+    def __init__(self, name):
+        self.alive = True
+        self.fed = False
+        self.name = name
+    def eat(self, food: Plant):
+        if food.edible:
+            self.fed = True
+            print(f'{self.name} съел {food.name}')
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            print(f'Фатальная ошибка. Попрощайтесь с {self.name}','\nТеперь он умрёт от голода.(')
+            self.alive = False
+
+class Mammal(Animal):
+    pass
+class Predator(Animal):
+    pass
+class Flower(Plant):
+    pass
+class Fruit(Plant):
+    def __init__(self, name):
+        self.edible = True
+        self.name = name
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
